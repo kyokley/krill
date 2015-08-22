9924f91bc2451a2c5de3e4d801a363aea971abbb
Kevin Yokley <kyokley2@gmail.com>
2015-08-22 13:38:49 -0500

    Adding parser modules

---
+++ krill/lexer.py
@@ -0,0 +1,42 @@
+# Based on the blog post at http://jayconrod.com/posts/37/a-simple-interpreter-from-scratch-in-python-part-1
+
+import sys
+import re
+
+RESERVED = 'RESERVED'
+KEYWORD = 'KEYWORD'
+
+token_exprs = [
+    (r'[ \n\t]+', None),
+    (r'#[^\n]*', None),
+    (r'\(', RESERVED),
+    (r'\)', RESERVED),
+    (r'&&', RESERVED),
+    (r'\|\|', RESERVED),
+    (r'(\S+\s*?)+?(?=\s*(\(|\)|&&|\|\|))', KEYWORD),
+    ]
+
+def lex(characters, token_exprs):
+    pos = 0
+    tokens = []
+    while pos < len(characters):
+        match = None
+        for token_expr in token_exprs:
+            pattern, tag = token_expr
+            regex = re.compile(pattern)
+            match = regex.match(characters, pos)
+            if match:
+                text = match.group(0)
+                if tag:
+                    token = (text, tag)
+                    tokens.append(token)
+                break
+        if not match:
+            print('Illegal character: %s\\n' % characters[pos:])
+            sys.exit(1)
+        else:
+            pos = match.end(0)
+    return tokens
+
+def filter_lex(characters):
+    return lex(characters, token_exprs)
