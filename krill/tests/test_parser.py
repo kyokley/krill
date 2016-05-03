b78d4af4bb04b51c24455e4eb1f2d6d12e8e9fe5
Kevin Yokley <kyokley2@gmail.com>
2016-05-02 19:17:11 -0500

    Using assertEqual in tests

--- krill/tests/test_parser.py
+++ krill/tests/test_parser.py
@@ -8,81 +8,81 @@ class TestParser(unittest.TestCase):
         tokens = filter_lex(test_str)
         expected = 'FilterExpr(python)'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_simple_with_parens(self):
         test_str = '(python )'
         tokens = filter_lex(test_str)
         expected = 'FilterExpr(python)'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_simple_with_spaces(self):
         test_str = 'python is the best'
         tokens = filter_lex(test_str)
         expected = 'FilterExpr(python is the best)'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_simple_AND(self):
         test_str = 'python is fun && python is simple'
         tokens = filter_lex(test_str)
         expected = 'AndExpr(FilterExpr(python is fun), FilterExpr(python is simple))'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_simple_OR(self):
         test_str = 'python is fun || python is the best'
         tokens = filter_lex(test_str)
         expected = 'OrExpr(FilterExpr(python is fun), FilterExpr(python is the best))'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_multi_AND(self):
         test_str = 'python is fun && python is simple && python is the best'
         tokens = filter_lex(test_str)
         expected = 'AndExpr(AndExpr(FilterExpr(python is fun), FilterExpr(python is simple)), FilterExpr(python is the best))'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_multi_OR(self):
         test_str = 'python is fun || python is simple || python is the best'
         tokens = filter_lex(test_str)
         expected = 'OrExpr(OrExpr(FilterExpr(python is fun), FilterExpr(python is simple)), FilterExpr(python is the best))'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_mixed_AND_and_OR(self):
         test_str = 'python is fun && python is simple || python is the best'
         tokens = filter_lex(test_str)
         expected = 'OrExpr(AndExpr(FilterExpr(python is fun), FilterExpr(python is simple)), FilterExpr(python is the best))'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_mixed_grouping(self):
         test_str = 'python is fun && (python is simple || python is the best)'
         tokens = filter_lex(test_str)
         expected = 'AndExpr(FilterExpr(python is fun), OrExpr(FilterExpr(python is simple), FilterExpr(python is the best)))'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_mixed_grouping_not(self):
         test_str = '!python is not fun && (python is simple || python is the best)'
         tokens = filter_lex(test_str)
         expected = 'AndExpr(NotExpr(FilterExpr(python is not fun)), OrExpr(FilterExpr(python is simple), FilterExpr(python is the best)))'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_multi_tier(self):
         test_str = 'a && (b || (c && d))'
         tokens = filter_lex(test_str)
         expected = 'AndExpr(FilterExpr(a), OrExpr(FilterExpr(b), AndExpr(FilterExpr(c), FilterExpr(d))))'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_quoted_filter(self):
         test_str = "a && (b || (c && d)) || '(e)'"
         tokens = filter_lex(test_str)
         expected = 'OrExpr(AndExpr(FilterExpr(a), OrExpr(FilterExpr(b), AndExpr(FilterExpr(c), FilterExpr(d)))), QuotedFilterExpr((e)))'
         actual = str(TokenParser(tokens).E())
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
