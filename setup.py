dbd08def13ce6e70b45ee8d7f822aaf7f523e312
Kevin Yokley <kyokley2@gmail.com>
2016-05-03 08:13:58 -0500

    Incrementing version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="krillpp",
 
-    version="0.4.0",
+    version="0.4.1",
 
     description="Read and filter web feeds",
     long_description="For a detailed description, see https://github.com/kyokley/krill.",
