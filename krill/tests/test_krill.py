ff5e7bfd75c8f47e203f37f295e4bfbca76f09b3
Kevin Yokley <kyokley@mars>
2024-10-10 20:17:00 -0500

    Convert tests to pytest

--- krill/tests/test_krill.py
+++ krill/tests/test_krill.py
@@ -1,61 +1,62 @@
-import unittest
+import pytest
 
 from unittest import mock
 from krill.krill import Application, fix_html
 import builtins
 
 
-class TestFixLinks(unittest.TestCase):
-    def test_fix_http(self):
+class TestFixLinks:
+    async def test_fix_http(self):
         test_str = 'This is a link.http://twitter.com'
 
         expected = 'This is a link. http://twitter.com'
-        actual = fix_html(test_str)
+        actual = await fix_html(test_str)
 
-        self.assertEqual(expected, actual)
+        assert expected == actual
 
-    def test_http_start_of_line(self):
+    async def test_http_start_of_line(self):
         test_str = 'http://twitter.com'
 
         expected = 'http://twitter.com'
-        actual = fix_html(test_str)
+        actual = await fix_html(test_str)
 
-        self.assertEqual(expected, actual)
+        assert expected == actual
 
-    def test_https_start_of_line(self):
+    async def test_https_start_of_line(self):
         test_str = 'https://twitter.com'
 
         expected = 'https://twitter.com'
-        actual = fix_html(test_str)
+        actual = await fix_html(test_str)
 
-        self.assertEqual(expected, actual)
+        assert expected == actual
 
-    def test_fix_https(self):
+    async def test_fix_https(self):
         test_str = 'This is a link.https://twitter.com'
 
         expected = 'This is a link. https://twitter.com'
-        actual = fix_html(test_str)
+        actual = await fix_html(test_str)
 
-        self.assertEqual(expected, actual)
+        assert expected == actual
 
-    def test_fix_link(self):
+    async def test_fix_link(self):
         test_str = 'This is a link.pic.twitter.com'
         expected = 'This is a link. pic.twitter.com'
-        actual = fix_html(test_str)
+        actual = await fix_html(test_str)
 
-        self.assertEqual(expected, actual)
+        assert expected == actual
 
-    def test_link_start_of_line(self):
+    async def test_link_start_of_line(self):
         test_str = 'pic.twitter.com'
 
         expected = 'pic.twitter.com'
-        actual = fix_html(test_str)
+        actual = await fix_html(test_str)
 
-        self.assertEqual(expected, actual)
+        assert expected == actual
 
 
-class TestReadSourceFile(unittest.TestCase):
-    def setUp(self):
+class TestReadSourceFile:
+    @pytest.fixture(autouse=True)
+    async def setUp(self):
         self.mock_readlines_sources = [
             'https://twitter.com/hashtag/programming python',
             'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml bailout',
@@ -73,15 +74,15 @@ class TestReadSourceFile(unittest.TestCase):
         self.application = Application(self.args)
 
     @mock.patch.object(builtins, 'open')
-    def test_read_filters_file(self, mock_open):
+    async def test_read_filters_file(self, mock_open):
         self.mock_myfile.readlines.return_value = self.mock_readlines_filters
         mock_open.return_value.__enter__.return_value = self.mock_myfile
         actual = self.application._read_filters_file('test')
         expected = ['python', 'programming', 'esm bailout', 'new horizons']
-        self.assertEqual(actual, expected)
+        assert expected == actual
 
     @mock.patch.object(builtins, 'open')
-    def test_read_source_file(self, mock_open):
+    async def test_read_source_file(self, mock_open):
         self.mock_myfile.readlines.return_value = self.mock_readlines_sources
         mock_open.return_value.__enter__.return_value = self.mock_myfile
         actual = self.application._read_sources_file('filename')
@@ -89,4 +90,4 @@ class TestReadSourceFile(unittest.TestCase):
             'https://twitter.com/hashtag/programming': 'python',
             'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml': 'bailout',
         }
-        self.assertEqual(actual, expected)
+        assert expected == actual
