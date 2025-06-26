a87c111d0a15e230d7a51b3041d865a8fe5db1e1
Kevin Yokley <kyokley2@gmail.com>
2025-06-26 08:46:52 -0500

    Add logging context manager

--- src/krill/utils.py
+++ src/krill/utils.py
@@ -1,3 +1,5 @@
+import sys
+from contextlib import contextmanager
 from datetime import datetime, timedelta, timezone
 
 FILTER_LAST_DAYS = 90
@@ -12,3 +14,22 @@ def validate_timestamp(timestamp):
     else:
         last_days_cutoff = datetime.now() - timedelta(days=FILTER_LAST_DAYS)
     return timestamp > last_days_cutoff
+
+
+@contextmanager
+def time_log(msg, debug=False):
+    start = datetime.now()
+
+    if debug:
+        sys.stdout.write(f"Start {msg}")
+        sys.stdout.flush()
+
+    try:
+        yield
+    except:
+        raise
+    finally:
+        finish = datetime.now()
+        if debug:
+            sys.stdout.write(f"Finish {msg} in {finish - start}")
+            sys.stdout.flush()
